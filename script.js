6607a473bffb71141f72ce4a1da49fb2d77f1b57
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-05-15 01:18:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
-// your code here
+	a = 0;
+	b = 1;
+   
+	for(let i= 0; i<=num; i++){
+		let sum = 0;
+		   console.log(a);
+	       console.log(b);
+		 a = b;
+		b = c;
+		c = a+b;
+		sum = sum + c;
+		console.log(sum);
+	}
 }
 
 module.exports = fibonacci;
