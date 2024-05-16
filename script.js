f4283448a4c3aa79d72d06e753e9c50c3109fae0
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-05-17 02:21:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function fibonacci(num) {
-	 nction fibonacci(num) {
+    if (num <= 1) {
+        return num;
+    }
+    
     let a = 0;
     let b = 1;
    
-    for(let i= 2; i<=num; i++){
+    for (let i = 2; i < num; i++) {
         let temp = a;
         a = b;
         b = temp + b;
     }
 	
-    return a;
-	 
-	
+    return b;
 }
+
 
 module.exports = fibonacci;
